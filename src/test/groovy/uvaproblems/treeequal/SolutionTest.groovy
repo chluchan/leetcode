dd09fa8aa85feef3ddb9b5358a7db9b0a68767b0
Chris Hluchan <chluchan@unifocus.com>
2016-08-16 13:09:24 -0600

    added same tree

---
+++ src/test/groovy/uvaproblems/treeequal/SolutionTest.groovy
@@ -0,0 +1,33 @@
+package uvaproblems.treeequal
+
+import spock.lang.Specification
+import uvaproblems.treeequal.Solution.TreeNode
+
+class SolutionTest extends Specification {
+   Solution solution = new Solution()
+
+   def "determines if two trees are structurely equal"() {
+      expect:
+      solution.isSameTree(tree1, tree2) == areEqual
+
+      where:
+      tree1               | tree2             || areEqual
+      null                | null              || true
+      new TreeNode(1)     | null              || false
+      null                | new TreeNode(1)   || false
+      new TreeNode(1)     | new TreeNode(1)   || true
+      new TreeNode(2)     | new TreeNode(1)   || false
+      tn(1, tn(1), null)  | tn(1)             || false
+      tn(1, tn(1), tn(2)) | tn(1, tn(1), tn(3)) || false
+      tn(1, tn(1, tn(3), null), tn(2)) | tn(1, tn(1, tn(3), null), tn(2)) || true
+      tn(1, tn(1, tn(3), tn(1)), tn(2)) | tn(1, tn(1, tn(3), null), tn(2)) || false
+   }
+
+   private TreeNode tn(value) {
+      return new TreeNode(value)
+   }
+
+   private TreeNode tn(value, left, right) {
+      return new TreeNode(value, left, right)
+   }
+}
