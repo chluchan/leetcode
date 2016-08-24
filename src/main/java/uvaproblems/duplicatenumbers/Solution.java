8b7ea642ff9730acb383bee95b9ca87aa31518fe
Chris Hluchan <chluchan@unifocus.com>
2016-08-24 17:23:04 -0600

    added find duplicate numbers 1-n

---
+++ src/main/java/uvaproblems/duplicatenumbers/Solution.java
@@ -0,0 +1,32 @@
+package uvaproblems.duplicatenumbers;
+
+// https://leetcode.com/problems/find-the-duplicate-number/
+public class Solution {
+   public int findDuplicate(int[] nums) {
+      if (nums.length <= 1) {
+         return -1;
+      }
+
+      int n = nums.length - 1;
+
+      int start = 0;
+      int end = n;
+      while (end - start > 1) {
+         int mid = (start + end) / 2;
+         int numLessOrEqualToMid = 0;
+         for (int num : nums) {
+            if (num <= mid) {
+               numLessOrEqualToMid++;
+            }
+         }
+
+         if (numLessOrEqualToMid > mid) {
+            end = mid;
+         } else {
+            start = mid;
+         }
+      }
+
+      return end;
+   }
+}
