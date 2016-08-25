d87c30283d31d69d0dff4712886428b4586cd58f
Chris Hluchan <chluchan@unifocus.com>
2016-08-24 18:27:48 -0600

    added rainwater

---
+++ src/test/groovy/uvaproblems/rainwater/SolutionTest.groovy
@@ -0,0 +1,20 @@
+package uvaproblems.rainwater
+
+import spock.lang.Specification
+
+class SolutionTest extends Specification {
+   Solution solution = new Solution()
+
+   def "Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it is able to trap after raining."() {
+      expect:
+      solution.trap(heights as int[]) == water
+
+      where:
+      heights      | water
+      []           | 0
+      [1]          | 0
+      [1, 2]       | 0
+      [2, 1, 2]    | 1
+      [6, 2, 1, 3] | 3
+   }
+}
